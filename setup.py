20821d835e893a7c9eaeb2e53133f81a5a12634d
Luan Fernandes <luangabriel70@gmail.com>
2023-07-18 11:36:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="janus",
     packages=find_packages(include=["janus", "janus.stats", "janus.utils"]),
-    version="0.1.1",
+    version="0.2.0",
     description="Janus, an A/B Test Framework",
     author="Luan Fernandes",
     license="",
